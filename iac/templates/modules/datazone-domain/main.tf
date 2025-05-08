b2c2c2a6b59b5ca20fc2f931500cf4941b29edce
RAVI PAWAR <RAVI_PAWAR@CRGL-THIRDPARTY.COM>
2025-05-09 00:51:30 +0530

    MIW

--- iac/templates/modules/datazone-domain/main.tf
+++ iac/templates/modules/datazone-domain/main.tf
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: MIT-0
 
 resource "awscc_datazone_domain" "domain" {
-
   name                  = var.DOMAIN_NAME
   domain_execution_role = var.DOMAIN_EXECUTION_ROLE_ARN
 
@@ -21,5 +20,16 @@ resource "aws_ssm_parameter" "domain_id" {
     Application = var.APP
     Environment = var.ENV
     Usage       = var.USAGE
+  }
+}
+
+resource "aws_sagemaker_domain" "example" {
+  domain_name = "smlh-pilot-v2-sgdomain"
+  auth_mode   = "IAM"
+  vpc_id      = "vpc-07e447180f989a1cd"
+  subnet_ids  = ["subnet-022a3b61e0427a553", "subnet-03e9fb7f85cae3127", "subnet-0cd269668f9b0f230"]
+
+  default_user_settings {
+    execution_role = var.DOMAIN_EXECUTION_ROLE_ARN
   }
 }
